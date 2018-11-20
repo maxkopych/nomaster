de2f1f7d23dd9c629b86b520269129ebb776018d
Max Kopych <max@cubicleninjas.com>
2018-11-20 16:36:15 -0600

    Adding form

---
+++ src/components/Form.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container my-5">
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Title</label>
+        <input type="email" v-model="title" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Note:</label>
+        <textarea id="exampleInputPassword1" v-model="text" class="form-control"></textarea>
+      </div>
+      <button @click="sendToParent" type="submit" class="btn btn-primary">Create</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Form",
+    data(){
+      return {
+        title: "",
+        text: ""
+      }
+    },
+    methods:{
+      sendToParent(){
+        const note={
+          title: this.title,
+          text: this.text,
+        };
+        this.title="";
+        this.text="";
+        this.$emit("submited",note);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
