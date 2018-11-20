78f3ed1d5855d5420ec8e1887d62811c117ec880
Max Kopych <max@cubicleninjas.com>
2018-11-20 17:39:24 -0600

    Changing Title

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,7 +3,7 @@
     <form>
       <div class="form-group">
         <label for="exampleInputEmail1">Title</label>
-        <input type="email" v-model="title" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <input type="email" v-model="title" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Note:</label>
